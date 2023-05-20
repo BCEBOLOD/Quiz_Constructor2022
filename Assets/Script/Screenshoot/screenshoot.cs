5a4aac3093251d3aeeb9bdfb4fd36838e4b7bcb3
BCEBOLOD <84376503+BCEBOLOD@users.noreply.github.com>
2023-05-20 14:33:40 +0300

    0.0.0.12
    почти релиз

---
+++ Assets/Script/Screenshoot/screenshoot.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class screenshoot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            ScreenCapture.CaptureScreenshot("screenshot.png");
+            Debug.Log("A screenshot was taken!");
+        }
+        //if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        //{
+        //    ScreenCapture.CaptureScreenshot("screenshot.png");
+        //    Debug.Log("A screenshot was taken!");
+        //}
+    }
+}
